9f18f14917fa51c2ed7d3657901057993635bbbb
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-09-12 21:12:21 +0300

    Added lists solution

--- lists/lists.py
+++ lists/lists.py
@@ -1,23 +1,34 @@
 class ListExercise:
     @staticmethod
-    def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
-
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
-        pass
+    def get_max_num(input_list: list[int]) -> int:
+        if not input_list:
+            return None
+        max_num = input_list[0]
+        for num in input_list:
+            if num > max_num:
+                max_num = num
+        return max_num
 
     @staticmethod
-    def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
+    def replace(input_list: list[int]) -> list[int]:
+        max_num = ListExercise.get_max_num(input_list)
+        return [max_num if num > 0 else num for num in input_list]
 
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
-        pass
+    @staticmethod
+    def search(input_list: list[int], query: int, first: int = 0, last: int = -1) -> int:
+        if last == -1:
+            last = len(input_list) - 1
+
+        if first > last or input_list[last] < query:
+            return -1
+
+        mid = (first + last) // 2
+
+        if input_list[mid] == query:
+            return mid
+        if input_list[mid] < query:
+            first = mid + 1
+        if input_list[mid] > query:
+            last = mid - 1
+
+        return ListExercise.search(input_list, query, first, last)
