a64ca8ce6e4d9ef541433de70c5afa604bdbe3c0
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-09-14 22:55:55 +0300

    Added flask solution

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -1,31 +1,38 @@
 from flask import Flask
+from flask import request, jsonify
 
 
 class FlaskExercise:
-    """
-    Вы должны создать API для обработки CRUD запросов.
-    В данной задаче все пользователи хранятся в одном словаре, где ключ - это имя пользователя,
-    а значение - его параметры. {"user1": {"age": 33}, "user2": {"age": 20}}
-    Словарь (dict) хранить в памяти, он должен быть пустым при старте flask.
-
-    POST /user - создание пользователя.
-    В теле запроса приходит JSON в формате {"name": <имя пользователя>}.
-    Ответ должен вернуться так же в JSON в формате {"data": "User <имя пользователя> is created!"}
-    со статусом 201.
-    Если в теле запроса не было ключа "name", то в ответ возвращается JSON
-    {"errors": {"name": "This field is required"}} со статусом 422
-
-    GET /user/<name> - чтение пользователя
-    В ответе должен вернуться JSON {"data": "My name is <name>"}. Статус 200
-
-    PATCH /user/<name> - обновление пользователя
-    В теле запроса приходит JSON в формате {"name": <new_name>}.
-    В ответе должен вернуться JSON {"data": "My name is <new_name>"}. Статус 200
-
-    DELETE /user/<name> - удаление пользователя
-    В ответ должен вернуться статус 204
-    """
+    users = {}
 
     @staticmethod
     def configure_routes(app: Flask) -> None:
-        pass
+        @app.post("/user")
+        def create_user():
+            data = request.get_json()
+
+            if "name" not in data:
+                return jsonify({"errors": {"name": "This field is required"}}), 422
+
+            user_name = data["name"]
+            FlaskExercise.users[user_name] = data
+            return jsonify({"data": f"User {user_name} is created!"}), 201
+
+        @app.get("/user/<name>")
+        def get_user(name):
+            user = FlaskExercise.users.get(name)
+            if user:
+                return jsonify({"data": f"My name is {user['name']}"}), 200
+            else:
+                return "", 404
+
+        @app.patch("/user/<name>")
+        def update_user(name):
+            data = request.get_json()
+            new_name = data["name"]
+            return jsonify({"data": f"My name is {new_name}"}), 200
+
+        @app.delete("/user/<name>")
+        def delete_user(name):
+            FlaskExercise.users.pop(name)
+            return "", 204
